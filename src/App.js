94661bf6963f70be2b8c0198d2953743be81f50e
jayaprabu <jayaprabu.jj@gmail.com>
2023-02-12 02:23:21 +0100

    Add lazy load in route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import Header from "./Component/Header";
+import LazyLoading from "./Component/lazyLoading";
 import StateManagement from "./Component/StateManagement";
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -9,10 +10,12 @@ import "bootstrap/dist/css/bootstrap.min.css"
 const App = () => {
   return ( <BrowserRouter>
     <Header/>
+    <div className="container mt-5">
     <Routes>
       <Route path="/state" element={<StateManagement/>}/>
-      <Route path="/lazy" element={"Lazy Loading"}/>
+      <Route path="/lazy" element={<LazyLoading/>}/>
     </Routes>
+    </div>
  </BrowserRouter>
  )
 }
